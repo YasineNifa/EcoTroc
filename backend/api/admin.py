868644ea4465a884ef1d63f24d1411c974034e71
ynifa <yasine.nifa@gmail.com>
2025-08-17 22:31:51 +0200

    Register model to the admin site.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from api.models import Profile, Listing, Transaction, Conversation, Message
+from api.models import (
+    Category,
+    Conversation,
+    Listing,
+    Message,
+    Profile,
+    Review,
+    Transaction,
+)
 
 
-admin.site.register(Profile)
-admin.site.register(Listing)
-admin.site.register(Transaction)
+admin.site.register(Category)
 admin.site.register(Conversation)
+admin.site.register(Listing)
 admin.site.register(Message)
+admin.site.register(Profile)
+admin.site.register(Review)
+admin.site.register(Transaction)
