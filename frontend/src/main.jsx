0c746fb21900056a17a3372651ba153e78f0c6d8
ynifa <yasine.nifa@gmail.com>
2025-08-19 20:30:40 +0200

    Add the SnackbarProvider for popup messages.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AuthContextProvider from "./context/AuthContextProvider.jsx";
+import { SnackbarProvider } from "notistack";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <SnackbarProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </SnackbarProvider>
   </StrictMode>
 );
