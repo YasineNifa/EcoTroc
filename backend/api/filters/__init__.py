0067b803969ac3a4fad7f82576a6dfa9d4e8ff08
ynifa <yasine.nifa@gmail.com>
2025-08-20 08:38:43 +0200

    Create 'PropositionFilter'.

--- backend/api/filters/__init__.py
+++ backend/api/filters/__init__.py
@@ -1,6 +1,8 @@
 from api.filters.listing import ListingFilter
+from api.filters.proposition import PropositionFilter
 
 
 __all__ = [
-    "ListingFilter"
+    "ListingFilter",
+    "PropositionFilter",
 ]
