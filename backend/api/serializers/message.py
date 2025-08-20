6f5b64aac2576713e8871a86fa560019c8bc6440
ynifa <yasine.nifa@gmail.com>
2025-08-20 08:39:58 +0200

    Add the proposition to the MessageSerializer.

--- backend/api/serializers/message.py
+++ backend/api/serializers/message.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 
 from api.models import Message
 from api.serializers.profile import ProfileSerializer
+from api.serializers.proposition import PropositionSerializer
 
 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = ProfileSerializer(read_only=True)
+    proposition = PropositionSerializer(read_only=True)
 
     class Meta:
         model = Message
-        fields = ["id", "sender", "content", "timestamp", "conversation", "message_type"]
-        read_only_fields = ["id", "sender", "timestamp", "conversation", "message_type"]
+        fields = ["id", "sender", "content", "timestamp", "conversation", "message_type", "proposition"]
+        read_only_fields = ["id", "sender", "timestamp", "conversation", "message_type", "proposition"]
