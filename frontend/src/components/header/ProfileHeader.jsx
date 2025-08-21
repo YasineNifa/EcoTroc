d6c99e4bfeedb652d06702805f4747cd189e0691
ynifa <yasine.nifa@gmail.com>
2025-08-21 14:09:35 +0200

    Redesigne the ProfileHeader component.

--- frontend/src/components/header/ProfileHeader.jsx
+++ frontend/src/components/header/ProfileHeader.jsx
@@ -1,25 +1,11 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import BioGeneratorModal from "../geminiPowered/BioGeneratorModal";
 import Button from "../ui/Button";
 import Icon from "../ui/Icon";
-import { AuthContext } from "../../context/AuthContextProvider";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import StarIcon from "@mui/icons-material/Star";
 
-// const mockUser = {
-//     username: "ya3399",
-//     avatarUrl: "https://placehold.co/128x128/E2E8F0/4A5568?text=Y",
-//     rating: 4.9,
-//     reviewsCount: 97,
-//     location: "Annouville, France",
-//     lastSeen: "Connected 33 minutes ago",
-//     followers: 21,
-//     following: 4,
-//     isVerified: true,
-//     bio: "Welcome to my shop! I carefully select and maintain all items for quality...",
-//   };
-const ProfileHeader = () => {
-  const { profile, user } = useContext(AuthContext);
+const ProfileHeader = ({ profile, navigaterProfile }) => {
   const [isBioModalOpen, setBioModalOpen] = useState(false);
   const [currentBio, setCurrentBio] = useState(profile?.bio);
 
@@ -31,48 +17,52 @@ const ProfileHeader = () => {
             src={
               profile?.image
                 ? profile?.image
-                : `https://placehold.co/40x40/E2E8F0/4A5568?text=${user?.username}`
+                : `https://placehold.co/40x40/E2E8F0/4A5568?text=${profile?.user?.username}`
             }
-            alt={user.username}
+            alt={profile?.user?.username}
             className="w-24 h-24 md:w-32 md:h-32 rounded-full"
           />
-          <div className="flex-1 text-center md:text-left">
-            <div className="flex items-center justify-center md:justify-start gap-4">
-              <h1 className="text-2xl font-bold">{user.username}</h1>
-              <Button variant="secondary">Edit Profile</Button>
+          <div className="flex-1 text-end flex justify-between">
+            <div className="flex justify-start flex-col items-start">
+              <h1 className="text-2xl font-bold">{profile?.user?.username}</h1>
+              <div className="flex items-center justify-center md:justify-start gap-1 text-yellow-500 my-2">
+                {[...Array(5)].map((_, i) => (
+                  <Icon key={i} className="!text-base">
+                    {i < Math.round(profile?.rating) ? (
+                      <StarIcon />
+                    ) : (
+                      <StarBorderIcon />
+                    )}
+                  </Icon>
+                ))}
+                <span className="text-gray-600 text-sm ml-1">
+                  {profile?.reviewsCount} reviews
+                </span>
+              </div>
+              <p className="text-sm text-gray-700 mt-6 text-center md:text-left">
+                {profile?.bio}
+              </p>
             </div>
-            <div className="flex items-center justify-center md:justify-start gap-1 text-yellow-500 my-2">
-              {[...Array(5)].map((_, i) => (
-                <Icon key={i} className="!text-base">
-                  {i < Math.round(user.rating) ? (
-                    <StarIcon />
-                  ) : (
-                    <StarBorderIcon />
-                  )}
-                </Icon>
-              ))}
-              <span className="text-gray-600 text-sm ml-1">
-                {user.reviewsCount} reviews
-              </span>
+            <div>
+              {profile?.id === navigaterProfile?.id && (
+                <>
+                  <Button variant="secondary" className="mr-1">
+                    Edit Profile
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    onClick={() => setBioModalOpen(true)}
+                    // className="mt-2 text-xs !px-2 !py-1"
+                  >
+                    ✨ Generate Bio with AI
+                  </Button>
+                </>
+              )}
             </div>
           </div>
         </div>
-        <div className="text-sm text-gray-700 mt-6 text-center md:text-left">
-          <p>
-            {currentBio}{" "}
-            <a href="#" className="text-teal-600 font-semibold">
-              View more
-            </a>
-          </p>
-          <Button
-            variant="secondary"
-            onClick={() => setBioModalOpen(true)}
-            className="mt-2 text-xs !px-2 !py-1"
-          >
-            ✨ Generate Bio with AI
-          </Button>
-        </div>
       </div>
+
       <BioGeneratorModal
         isOpen={isBioModalOpen}
         onClose={() => setBioModalOpen(false)}
