f6a7d86526bca82c72f9436df8ef5979103da2d3
ynifa <yasine.nifa@gmail.com>
2025-08-25 20:07:48 +0200

    Add a left margin to the Token Component.

--- frontend/src/components/ui/Token.jsx
+++ frontend/src/components/ui/Token.jsx
@@ -1,3 +1,5 @@
-const Token = () => <span className="text-yellow-500 font-bold ml-1">🪙</span>;
+const Token = () => (
+  <span className="text-yellow-500 font-bold ml-1.5">🪙</span>
+);
 
 export default Token;
