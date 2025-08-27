1f3f4fbe11922bdaec3b0b7fd4caffa585db7ec9
ynifa <yasine.nifa@gmail.com>
2025-08-27 08:52:25 +0200

    Add the 'has_unread_messages' and 'last_message' fields to the ConversationSerializer.

--- backend/api/serializers/conversation.py
+++ backend/api/serializers/conversation.py
@@ -10,9 +10,18 @@ class ConversationSerializer(serializers.ModelSerializer):
     participants = ProfileSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
     listing = ListingSerializer(read_only=True)
+    has_unread_messages = serializers.SerializerMethodField()
+    last_message = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ["id", "listing", "participants", "messages", "created_at"]
-        # fields = ['id', 'listing', 'participants', 'messages']
+        fields = ["id", "listing", "participants", "messages", "created_at", "has_unread_messages", "last_message"]
         read_only_fields = ["id", "created_at", "listing", "participants", "messages"]
+
+    def get_has_unread_messages(self, obj):
+        user = self.context["request"].user.profile
+        return obj.messages.filter(is_read=False).exclude(sender=user).exists()
+    
+    def get_last_message(self, obj):
+        last_message = obj.messages.order_by("-timestamp").first()
+        return MessageSerializer(last_message).data if last_message else None
