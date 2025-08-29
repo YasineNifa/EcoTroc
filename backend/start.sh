44bbf4c24ffaa25d5f3bfc129e1c364c632c9d1a
ynifa <yasine.nifa@gmail.com>
2025-08-29 15:09:23 +0200

    Remove Fly settings

--- backend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "--- Starting the ultimate debug script ---"
-echo "--- If you see this message, the container has started successfully. ---"
-echo "--- The script will now sleep indefinitely to keep the container running. ---"
-
-# This command will prevent the container from exiting.
-sleep infinity
