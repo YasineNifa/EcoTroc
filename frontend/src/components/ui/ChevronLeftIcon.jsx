39d9a253c0bdde6d48b4713c35ed9ca031ac60c9
ynifa <yasine.nifa@gmail.com>
2025-09-01 15:04:25 +0200

    Create ChevronLeftIcon

---
+++ frontend/src/components/ui/ChevronLeftIcon.jsx
@@ -0,0 +1,18 @@
+const ChevronLeftIcon = () => (
+    <svg
+      className="w-8 h-8"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15 19l-7-7 7-7"
+      />
+    </svg>
+  );
+  
+  export default ChevronLeftIcon;
