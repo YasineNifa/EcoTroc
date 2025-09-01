bcdbaeefd517347a6ad57f404571ab1fc0179a5e
ynifa <yasine.nifa@gmail.com>
2025-09-01 16:19:18 +0200

    Remove comment

--- frontend/src/helpers/axios/getCommonOptions.js
+++ frontend/src/helpers/axios/getCommonOptions.js
@@ -1,13 +1,8 @@
 function getCommonOptions(options = {}) {
   const { isFormData = false } = options;
-  // const authToken = localStorage.getItem("authToken");
   const headers = {
     "Content-Type": isFormData ? "multipart/form-data" : "application/json",
   };
-  // if (authToken) {
-  //   headers["Authorization"] = `Token ${authToken}`;
-  // }
-
   return { headers };
 }
 
