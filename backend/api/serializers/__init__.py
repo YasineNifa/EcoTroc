8c763c120b6987b78e46f5f270450fb16a395562
ynifa <yasine.nifa@gmail.com>
2025-09-02 15:13:24 +0200

    Create the BrandSerializer.

--- backend/api/serializers/__init__.py
+++ backend/api/serializers/__init__.py
@@ -1,3 +1,4 @@
+from api.serializers.brand import BrandSerializer
 from api.serializers.category import CategorySerializer
 from api.serializers.conversation import ConversationSerializer
 from api.serializers.listing import ListingSerializer
@@ -12,6 +13,7 @@ from api.serializers.user import UserSerializer
 
 
 __all__ = [
+    "BrandSerializer",
     "CategorySerializer",
     "ConversationSerializer",
     "ListingSerializer",
