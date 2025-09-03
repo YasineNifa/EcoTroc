4574b7edc59628f7d6e790cb3d35da81a0f60b31
ynifa <yasine.nifa@gmail.com>
2025-09-03 15:43:18 +0200

    Remove the Link Component and use the parent Link

--- frontend/src/components/ListListings.jsx
+++ frontend/src/components/ListListings.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ItemCard from "./shared/ItemCard";
-import { Link } from "react-router-dom";
 
 function ListListings({ results }) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
       {results.map((item) => (
-        <Link key={item.id} to={`/listings/${item.id}`}>
-          <ItemCard item={item} />
-        </Link>
+        <ItemCard item={item} key={item.id} />
       ))}
     </div>
   );
