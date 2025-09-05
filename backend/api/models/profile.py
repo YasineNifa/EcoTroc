a5d9ede11d24c54cc2cb8e4c7eaa32d98c3b33ad
ynifa <yasine.nifa@gmail.com>
2025-09-05 15:08:18 +0200

    Add new fields to the Profile model + Generate migrations

--- backend/api/models/profile.py
+++ backend/api/models/profile.py
@@ -1,3 +1,5 @@
+from django_countries.fields import CountryField
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -10,6 +12,16 @@ class Profile(models.Model):
     locked_jetons = models.PositiveIntegerField(default=0)
     bio = models.TextField(blank=True)
     image = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    country = CountryField("Country", blank=True, null=True)
+    city = models.CharField("City", max_length=100, blank=True)
+    show_city_in_profile = models.BooleanField("Display city in profile", default=True)
+    # preferred_language = models.CharField("Langue", max_length=10, default='fr')
+    phone_number = models.CharField("Phone number", max_length=20, blank=True)
+    email_verified = models.BooleanField("Verified email", default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    last_login_at = models.DateTimeField("Last connexion", null=True, blank=True)
+
 
     def __str__(self):
         return self.user.username
