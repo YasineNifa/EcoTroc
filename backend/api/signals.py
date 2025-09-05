69c6e165f536611d72a45b667d1dbfcec60ffcae
ynifa <yasine.nifa@gmail.com>
2025-09-05 17:31:49 +0200

    Create the 'update_last_login_on_user_login' logic.

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -3,7 +3,9 @@ from asgiref.sync import async_to_sync
 
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
+from django.utils import timezone
 
 from api.models import Profile, Message, Notification, Proposition
 from api.serializers import NotificationSerializer
@@ -57,3 +59,12 @@ def create_proposition_notificaton(sender, instance, created, **kwargs):
             f"notifications_{instance.listing.owner.user.id}",
             {"type": "send_notification", "notification": serialized_notification},
         )
+
+@receiver(user_logged_in)
+def update_last_login_on_user_login(sender, user, request, **kwargs):
+    try:
+        profile = user.profile
+        profile.last_login_at = timezone.now()
+        profile.save(update_fields=['last_login_at'])
+    except Profile.DoesNotExist:
+        pass
